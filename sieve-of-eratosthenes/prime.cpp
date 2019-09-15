2a4abcc30412203279c5433dead5de66046229cd
LinKin-22 <shomnathcse22@gmail.com>
2019-09-15 23:54:42 +0600

    added iterative prime generator sieve function

---
+++ sieve-of-eratosthenes/prime.cpp
@@ -0,0 +1,19 @@
+
+const int MX  = 1e6 + 7;
+int vis[MX], prm[MX];
+int i, j, p;
+
+void sieve()
+{
+    prm[p++] = 2;
+    for(i = 3; i*i <= MX; i+=2){
+        if(!vis[i]){
+            prm[p++] = i;
+            for(j = i*i; j <= MX; j += (i<<1)) vis[j] = 1;
+        }
+    }
+    for(; i < MX; i+=2) if(!vis[i]) prm[p++] = i;
+
+}
+
+
